86cb80b1d83c5f40b2b4f6d6e0fb867d12649aec
Jon Smith <aardvarkjon94@yahoo.com>
2021-11-25 23:56:47 -0600

    Added movemnet

--- hello.py
+++ hello.py
@@ -2,11 +2,15 @@ from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
+from direct.interval.IntervalGlobal import Sequence
+from panda3d.core import Point3
 
 class MyApp(ShowBase):
 
     def __init__(self):
         ShowBase.__init__(self)
+
+        self.disableMouse()
 
         # Loading the Scene
         self.scene = self.loader.loadModel("models/environment")
@@ -23,6 +27,25 @@ class MyApp(ShowBase):
         self.pandaActor.setScale(0.005,0.005,0.005)
         self.pandaActor.reparentTo(self.render)
         self.pandaActor.loop("walk")
+
+        posInterval1 = self.pandaActor.posInterval(13,
+                                                   Point3(0, -10, 0),
+                                                   startPos=Point3(0, 10, 0))
+        posInterval2 = self.pandaActor.posInterval(13,
+                                                   Point3(0, 10, 0),
+                                                   startPos=Point3(0, -10, 0))
+        hprInterval1 = self.pandaActor.hprInterval(3,
+                                                   Point3(180, 0, 0),
+                                                   startHpr=Point3(0, 0, 0))
+        hprInterval2 = self.pandaActor.hprInterval(3,
+                                                   Point3(0, 0, 0),
+                                                   startHpr=Point3(180, 0, 0))
+
+        # Create and play the sequence that coordinates the intervals.
+        self.pandaPace = Sequence(posInterval1, hprInterval1,
+                                  posInterval2, hprInterval2,
+                                  name="pandaPace")
+        self.pandaPace.loop()
         # function to spin the camera
     def spinCameraTask(self, task):
         angleDegrees = task.time * 6.9
