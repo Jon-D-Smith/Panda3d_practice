9654f7cf6c446622879cf2b7ad11f2022caf7f7b
Jon Smith <aardvarkjon94@yahoo.com>
2021-11-25 21:46:02 -0600

    Loaded environment

--- hello.py
+++ hello.py
@@ -1,14 +1,16 @@
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import WindowProperties
 
-class Game(ShowBase):
+
+class MyApp(ShowBase):
+
     def __init__(self):
         ShowBase.__init__(self)
 
-        properties = WindowProperties()
-        properties.setSize(1000,750)
-        self.win.requestProperties(properties)
-        self.disableMouse()
+        # Loading the Scene
+        self.scene = self.loader.loadModel("models/environment")
+        self.scene.reparentTo(self.render)
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
 
-game = Game()
-game.run()
+app = MyApp()
+app.run()
