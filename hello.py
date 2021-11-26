f48a698735438ab852b09fdb6581ad43c80b62a2
Jon Smith <aardvarkjon94@yahoo.com>
2021-11-25 20:42:25 -0600

    finished hello world tutorial

---
+++ hello.py
@@ -0,0 +1,14 @@
+from direct.showbase.ShowBase import ShowBase
+from panda3d.core import WindowProperties
+
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        properties = WindowProperties()
+        properties.setSize(1000,750)
+        self.win.requestProperties(properties)
+        self.disableMouse()
+
+game = Game()
+game.run()
