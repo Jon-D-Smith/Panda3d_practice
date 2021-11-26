bdd5a2532ab54c9dbc9dbe1a60f681b29504baf5
Jon Smith <aardvarkjon94@yahoo.com>
2021-11-25 21:54:23 -0600

    Added camera rotation script

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
+from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
-
+from direct.task import Task
 
 class MyApp(ShowBase):
 
@@ -11,6 +12,17 @@ class MyApp(ShowBase):
         self.scene.reparentTo(self.render)
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
+
+        # Adding a spin camera Task
+        self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+
+        # function to spin the camera
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 6.9
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
 
 app = MyApp()
 app.run()
